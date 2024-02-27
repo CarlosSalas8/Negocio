e4040b4de2273d8d95c83275b894c8c96feac40c
CarlosSalas8 <92597874+CarlosSalas8@users.noreply.github.com>
2024-02-27 09:40:01 -0500

    first commit

---
+++ src/app/container1/container1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-container1',
+  templateUrl: './container1.component.html',
+  styleUrls: ['./container1.component.css']
+})
+export class Container1Component {
+  
+
+}
